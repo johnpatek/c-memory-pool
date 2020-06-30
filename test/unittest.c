b2ff34569ae57fdeceb6561fda707b0d267cf962
John Patek <johnpatek2@gmail.com>
2020-06-30 00:22:34 -0700

    Added all unit test definitions pending implementations

--- test/unittest.c
+++ test/unittest.c
@@ -1,32 +1,94 @@
 #include "memory_pool.h"
 #include <stdlib.h>
 
-int create_pool_test()
-{
-    int result;
-    struct memory_pool *pool;
-    pool = mp_create(1024);
-    result = (pool == NULL);
-    mp_destroy(pool);
-    return result;
-}
+static int mp_create_test();
 
-int destroy_pool_test()
-{
+static int mp_destroy_test();
 
-}
+static int mp_malloc_test();
+
+static int mp_calloc_test();
+
+static int mp_realloc_test();
+
+static int mp_free_test();
+
+static int mp_free_size_test();
+
+static int mp_total_size_test();
+
+static int mp_largest_block_size_test();
 
 int main(const int argc, const char ** argv)
 {
     int result;
     #if defined(MP_CREATE_TEST)
-        result = create_pool_test();
+        result = mp_create_test();
     #elif defined(MP_DESTROY_TEST)
-        result = destroy_pool_test();
+        result = mp_destroy_test();
     #elif defined (MP_MALLOC_TEST)
-
+        result = mp_malloc_test();
+    #elif defined (MP_CALLOC_TEST)
+        result = mp_calloc_test();
+    #elif defined (MP_REALLOC_TEST)
+        result = mp_realloc_test();
+    #elif defined (MP_FREE_TEST)
+        result = mp_free_test();
+    #elif defined (MP_FREE_SIZE_TEST)
+        result = mp_free_size_test();
+    #elif defined (MP_TOTAL_SIZE_TEST)
+        result = mp_total_size_test();
+    #elif defined (MP_LARGEST_BLOCK_SIZE_TEST)
+        result = mp_largest_block_size_test();
     #else
-        result = 0;
+    // Not a vald test case, should be unreachable.
+        result = -1;
     #endif
+
+    return result;
+}
+
+static int mp_create_test()
+{
+    return 0;
+}
+
+static int mp_destroy_test()
+{
+    return 0;
+}
+
+static int mp_malloc_test()
+{
+    return 0;
+}
+
+static int mp_calloc_test()
+{
+    return 0;
+}
+
+static int mp_realloc_test()
+{
+    return 0;
+}
+
+static int mp_free_test()
+{
+    return 0;
+}
+
+static int mp_free_size_test()
+{
+    return 0;
+}
+
+static int mp_total_size_test()
+{
+    return 0;
+}
+
+static int mp_largest_block_size_test()
+{
     return 0;
 }
